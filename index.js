0ce9c8c0623cd324f6722f49dce7e412641a16d7
Steve <kamaustevek78@gmail.com>
2023-03-12 16:04:31 +0300

    Completed assignmnet

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+const cards = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(cards) {
+    for(let i = 0; i < cards.length; i++) {
+        console.log(`Thank You, ${cards[i]}, for the wonderful surprise gift`);
+        debugger;
+    }
+
+    return cards;
+}
+
+writeCards(cards);
+
+function countdown() {
+    let countdown = 0;
+    while (countdown < 10) {
+        console.log(countdown++)
+    }
+}
